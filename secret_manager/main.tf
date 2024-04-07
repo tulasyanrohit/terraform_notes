0317ce793270c7828327402cdf91409c75bca40f
Rohit Tulasyan <rohittulasyan@Rohits-MacBook-Pro.local>
2024-04-07 22:41:14 +0530

    adding comments

--- secret_manager/main.tf
+++ secret_manager/main.tf
@@ -6,6 +6,7 @@ data aws_secretsmanager_secret_version credentials {
     secret_id = "some-secret-name"
 }
 
+#Using JsonDecode function to map "secret_string" to json object and assiging to local var
 locals {
     db_creds = jsondecode(data.aws_secretsmanager_secret_version.credentials.secret_string)
 }
@@ -16,6 +17,11 @@ resource "aws_db_instance" "example" {
   allocated_storage    = 10
   db_name              = "mydb"
   engine               = "mysql"
+  #Insted of using 
+  #username = admin
+  #password = assword
+
+  #Using aws secret manager to access the secret data
   username = local.db_creds.username
   password = local.db_creds.password
   skip_final_snapshot = true
